35a66ca57cf8690fc760609126344b64dc60384f
nishi_hiroyuki <nishi_hiroyuki@cyberagent.co.jp>
2024-04-10 15:10:53 +0900

    wip

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-import argparse
-import json
 import os
 
 import boto3
@@ -8,7 +6,6 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import BedrockChat
 from langchain.retrievers import AmazonKnowledgeBasesRetriever
 
-# os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
 bedrock_runtime = boto3.client('bedrock-runtime')
 load_dotenv('.env')
 KNOWLEDGE_BASE_ID = os.getenv('KNOWLEDGE_BASE_ID')
@@ -23,6 +20,8 @@ def knowledge(name: str):
             }
         )
 
+        print('--------debug: knowledge id--------')
+        print(KNOWLEDGE_BASE_ID)
         retriever = AmazonKnowledgeBasesRetriever(
             knowledge_base_id=KNOWLEDGE_BASE_ID,
             retrieval_config={
@@ -40,29 +39,24 @@ def knowledge(name: str):
         )
         query = f"{name}さんについて教えてください"
         print(query)
-        print('--------debug1--------')
         result = qa.run(query)
         # TODO: streamで返す？
-        print('--------debug2--------')
         return result
     except Exception as e:
         print(e)
 
 
-# def lambda_handler(event, context):
 def lambda_handler(event, context):
     print('---------START---------')
-    print(KNOWLEDGE_BASE_ID)
     # parser = argparse.ArgumentParser(description="Chatbot")
     # parser.add_argument("--about", type=str, default="西")
     # name = parser.parse_args()
     # langchainを使用した処理をここに記述
     # この例では、受け取ったイベントの内容をそのまま返します
     print('--------START: knowledge--------')
-    r = knowledge('西')
+    res = knowledge('西')
     print('--------END: knowledge--------')
-    print(r)
     return {
         'statusCode': 200,
-        'body': r
+        'body': res
     }
